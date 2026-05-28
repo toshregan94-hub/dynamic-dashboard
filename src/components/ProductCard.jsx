57be9047ab8661101509f87941f6b9e618b9d573
Regan Tosh <toshregan94@gmail.com>
2026-05-28 15:56:47 +0300

    Potential changes

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,6 @@
-import styles from '../styles/ProductCard.module.css';
-
 function ProductCard({ product, onRemove }) {
   return (
-    <div className={product.inStock ? styles.card : styles.outOfStockClass}>
+    <div className={product.inStock ? 'card' : 'outOfStockClass'}>
       <h2>{product.name}</h2>
       <p>Price: {product.price}</p>
       <p>{product.inStock ? 'In Stock' : 'Out of Stock'}</p>
